8cc3728cbd4398db245ac2293748af9973a79083
dattphan15 <ken.phan@live.ca>
2021-07-12 13:52:05 -0700

    add useEffect hook to send an alert when the thought expires

--- Thought.js
+++ Thought.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export function Thought(props) {
   const { thought, removeThought } = props;
+
+  useEffect(() => {
+    // calculates time until thought expires
+    const timeRemaining = thought.expiresAt - Date.now();
+    const timeout = setTimeout(() => {
+      alert('Time has passed!')
+    }, timeRemaining)
+    // clears timeout
+    return () => {clearTimeout(timeout)}
+    // re-renders the effect everytime the thought is different
+  }, [thought])
 
   const handleRemoveClick = () => {
     removeThought(thought.id);
